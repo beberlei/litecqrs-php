9912192686e071cbe8fb8e58f2fd85836ab7490a
Benjamin Eberlei <kontakt@beberlei.de>
2012-08-10 07:36:11 +0200

    Rename Doctrine DBAL/ORM Handlers to the MessageHandler namespace and fix implementation

--- src/LiteCQRS/Plugin/Doctrine/MessageHandler/DbalTransactionalHandler.php
+++ src/LiteCQRS/Plugin/Doctrine/MessageHandler/DbalTransactionalHandler.php
@@ -2,30 +2,30 @@
 
 namespace LiteCQRS\Plugin\Doctrine\CommandHandler;
 
-use LiteCQRS\CommandHandler\CommandHandlerInterface;
-use LiteCQRS\Command;
+use LiteCQRS\Bus\MessageHandlerInterface;
+use LiteCQRS\Bus\MessageInterface;
 
 use Exception;
 
 use Doctrine\DBAL\Connection;
 
-class DbalTransactionalHandler implements CommandHandlerInterface
+class DbalTransactionalHandler implements MessageHandlerInterface
 {
     private $conn;
     private $next;
 
-    public function __construct(Connection $conn, CommandHandlerInterface $next)
+    public function __construct(Connection $conn, MessageHandlerInterface $next)
     {
         $this->conn = $conn;
         $this->next = $next;
     }
 
-    public function handle(Command $command)
+    public function handle(MessageInterface $message)
     {
         $this->conn->beginTransaction();
 
         try {
-            $this->next->handle($command);
+            $this->next->handle($message);
             $this->next->commit();
         } catch(Exception $e) {
             $this->conn->rollBack();
--- src/LiteCQRS/Plugin/Doctrine/MessageHandler/OrmTransactionalHandler.php
+++ src/LiteCQRS/Plugin/Doctrine/MessageHandler/OrmTransactionalHandler.php
@@ -1,31 +1,31 @@
 <?php
 
-namespace LiteCQRS\Plugin\Doctrine\CommandHandler;
+namespace LiteCQRS\Plugin\Doctrine\MessageHandler;
 
-use LiteCQRS\CommandHandler\CommandHandlerInterface;
-use LiteCQRS\Command;
+use LiteCQRS\Bus\MessageHandlerInterface;
+use LiteCQRS\Bus\MessageInterface;
 
 use Exception;
 
 use Doctrine\ORM\EntityManager;
 
-class OrmTransactionalHandler implements CommandHandlerInterface
+class OrmTransactionalHandler implements MessageHandlerInterface
 {
     private $entityManager;
     private $next;
 
-    public function __construct(EntityManager $entityManager, CommandHandlerInterface $next)
+    public function __construct(EntityManager $entityManager, MessageHandlerInterface $next)
     {
         $this->entityManager = $entityManager;
         $this->next = $next;
     }
 
-    public function handle(Command $command)
+    public function handle(MessageInterface $message)
     {
         $this->entityManager->beginTransaction();
 
         try {
-            $this->next->handle($command);
+            $this->next->handle($message);
             $this->entityManager->flush();
             $this->entityManager->commit();
         } catch(Exception $e) {
