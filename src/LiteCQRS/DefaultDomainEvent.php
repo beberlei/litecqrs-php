d894857f1e1f8948732c59af70596884805b2598
Benjamin Eberlei <kontakt@beberlei.de>
2012-08-10 11:39:07 +0200

    Make DefaultDomainEvent easier to use

--- src/LiteCQRS/DefaultDomainEvent.php
+++ src/LiteCQRS/DefaultDomainEvent.php
@@ -3,6 +3,15 @@ namespace LiteCQRS;
 
 abstract class DefaultDomainEvent implements DomainEvent
 {
+    public function __construct(array $data = array())
+    {
+        foreach ($data as $key => $value) {
+            if (property_exists($this->$key)) {
+                $this->$key => $value;
+            }
+        }
+    }
+
     public function getEventName()
     {
         $class = get_class($class);
