372a6207183f196d3ce6af79a2c0b50dfd31e323
Benjamin Eberlei <kontakt@beberlei.de>
2012-08-20 23:08:32 +0200

    Bugfix

--- src/LiteCQRS/Plugin/Monolog/MonologDebugLogger.php
+++ src/LiteCQRS/Plugin/Monolog/MonologDebugLogger.php
@@ -33,7 +33,7 @@ class MonologDebugLogger implements MessageHandlerInterface
             $info  = end($parts);
         } else if ($message instanceof DomainEvent) {
             $log  = "Event[%s]: %s";
-            $info = $message->getEventName() . "/" . $event->getMessageHeader()->id;
+            $info = $message->getEventName() . "/" . $message->getMessageHeader()->id;
         }
 
         try {
