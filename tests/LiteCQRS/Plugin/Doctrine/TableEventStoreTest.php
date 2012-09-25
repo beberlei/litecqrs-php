85bd61b0fd7b5a0bda88118025007f04b99c9425
Joscha <joscha@schnipseljagd.org>
2012-09-25 19:32:47 +0200

    event store can handle aggregate ids with multiple keys

--- tests/LiteCQRS/Plugin/Doctrine/TableEventStoreTest.php
+++ tests/LiteCQRS/Plugin/Doctrine/TableEventStoreTest.php
@@ -32,4 +32,26 @@ class TableEventStoreTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals($event->getMessageHeader()->id, $data[0]['event_id']);
         $this->assertEquals('{}', $data[0]['data']);
     }
+
+    public function testStoreEventWithAggregateIdContainingMultipleKeys()
+    {
+        $serializer = $this->getMock('LiteCQRS\EventStore\SerializerInterface');
+        $serializer->expects($this->once())->method('serialize')->will($this->returnValue('{}'));
+
+        $conn = DriverManager::getConnection(array("driver" => "pdo_sqlite", "memory" => true));
+
+        $schema = new TableEventStoreSchema();
+        $tableSchema = $schema->getTableSchema();
+        $conn->getSchemaManager()->createTable($tableSchema);
+
+        $eventStore = new TableEventStore($conn, $serializer, $tableSchema->getName());
+
+        $event = new DomainObjectChanged("Test", array());
+        $event->getMessageHeader()->aggregateId = array('id' => 43, 'name' => 'test name');
+
+        $eventStore->store($event);
+
+        $data = $conn->fetchAll('SELECT * FROM litecqrs_events');
+        $this->assertEquals('{"id":43,"name":"test name"}', $data[0]['aggregate_id']);
+    }
 }
