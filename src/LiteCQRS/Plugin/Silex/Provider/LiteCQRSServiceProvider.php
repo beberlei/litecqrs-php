ddb163d412bfdd42639bafb200a394624bdad12e
Henrik Bjørnskov <henrik@bjrnskov.dk>
2012-12-29 15:44:31 +0100

    add automatical event handler registering based on parameter

--- src/LiteCQRS/Plugin/Silex/Provider/LiteCQRSServiceProvider.php
+++ src/LiteCQRS/Plugin/Silex/Provider/LiteCQRSServiceProvider.php
@@ -16,23 +16,22 @@ class LiteCQRSServiceProvider implements \Silex\ServiceProviderInterface
      */
     public function register(Application $app)
     {
-        $app['lite_cqrs.event_proxy_factories'] = array();
+        $app['lite_cqrs.event_proxy_factories']   = array();
         $app['lite_cqrs.command_proxy_factories'] = array();
+        $app['lite_cqrs.commands']                = array();
+        $app['lite_cqrs.event_handlers']          = array();
 
         $app['command_bus'] = $app->share(function (Application $app) {
             return new Silex\ApplicationCommandBus($app, $app['lite_cqrs.command_proxy_factories']);
         });
 
-        $app['lite_cqrs.commands'] = array();
-
         $app['lite_cqrs.identity_map'] = $app->share(function () {
             return new Bus\SimpleIdentityMap();
         });
 
-        $app['lite_cqrs.event_message_bus'] = $app->share(function () {
+        $app['lite_cqrs.event_message_bus'] = $app->share(function (Application $app) {
             return new Silex\ApplicationEventBus($app, $app['lite_cqrs.event_proxy_factories']);
         });
-
 
         $app['lite_cqrs.event_message_handler'] = $app->share(function (Application $app) {
             return new Bus\EventMessageHandlerFactory($app['lite_cqrs.event_message_bus'], $app['lite_cqrs.identity_map']);
@@ -49,5 +48,7 @@ class LiteCQRSServiceProvider implements \Silex\ServiceProviderInterface
     public function boot(Application $app)
     {
         $app['command_bus']->registerServices($app['lite_cqrs.commands']);
+
+        $app['lite_cqrs.event_message_bus']->registerServices($app['lite_cqrs.event_handlers']);
     }
 }
