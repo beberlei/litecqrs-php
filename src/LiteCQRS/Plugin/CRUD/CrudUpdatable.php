8a29ac5c34232b74b9b42c73aa50e6397d5d084c
Mark Badolato <mbadolato@cainandassociates.com>
2013-07-18 13:28:09 -0700

    Fix Crud traits to allow overriding which class properties may change

--- src/LiteCQRS/Plugin/CRUD/CrudUpdatable.php
+++ src/LiteCQRS/Plugin/CRUD/CrudUpdatable.php
@@ -24,12 +24,23 @@ trait CrudUpdatable
 
     protected function applyResourceUpdated(ResourceUpdatedEvent $event)
     {
-        $properties = array_keys(get_class_vars($this));
+        $properties = $this->getAccessibleProperties();
 
         foreach ($event->data as $key => $value) {
             if (in_array($key, $properties)) {
                 $this->$key = $value;
             }
         }
+    }
+
+    /**
+     * Return an array of properties that are allowed to change
+     * through the create() and update() methods.
+     *
+     * @return array
+     */
+    protected function getAccessibleProperties()
+    {
+        return array_keys(get_class_vars(get_class($this)));
     }
 }
