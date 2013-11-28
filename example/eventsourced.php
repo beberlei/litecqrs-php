9b981c0baa4c2e9a1ce9f31f4987a462fbfadaa1
Benjamin Eberlei <kontakt@beberlei.de>
2013-11-28 23:34:08 +0100

    Simple InventoryItem Example with 2.0 API

---
+++ example/eventsourced.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace EventSourced;
+
+use Rhumsaa\Uuid\Uuid;
+use LiteCQRS\DefaultDomainEvent;
+use LiteCQRS\AggregateRoot;
+
+require_once __DIR__ . "/../vendor/autoload.php";
+
+class InventoryItem extends AggregateRoot
+{
+    private $name;
+    private $activated = true;
+
+    public function __construct($id)
+    {
+        $this->apply(new InventoryItemCreated($id));
+    }
+
+    public function changeName($name)
+    {
+        $this->apply(new InventoryItemRenamed($this->getId(), $name));
+    }
+
+    public function deactivate()
+    {
+        $this->apply(new InventoryItemDeactivated($this->getId()));
+    }
+
+    protected function applyInventoryItemCreated($event)
+    {
+        $this->setId($event->id);
+    }
+
+    protected function applyInventoryItemRenamed($event)
+    {
+        $this->name = $event->name;
+    }
+
+    protected function applyInventoryItemDeactivated($event)
+    {
+        $this->activated = false;
+    }
+}
+
+class InventoryItemCreated extends DefaultDomainEvent
+{
+    protected $id;
+
+    public function __construct($id)
+    {
+        $this->id = $id;
+    }
+}
+
+class InventoryItemRenamed extends DefaultDomainEvent
+{
+    protected $id;
+    protected $name;
+
+    public function __construct($id, $name)
+    {
+        $this->id = $id;
+        $this->name = $name;
+    }
+}
+
+class InventoryItemDeactivated extends DefaultDomainEvent
+{
+    protected $id;
+
+    public function __construct($id)
+    {
+        $this->id = $id;
+    }
+}
+
+$item = new InventoryItem(Uuid::uuid4());
+$item->changeName("Foo");
+$item->deactivate();
+
+var_dump($item);
