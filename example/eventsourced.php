06ddfad3e653f8d4846566072c9caad1903a3aaa
Benjamin Eberlei <kontakt@beberlei.de>
2013-11-29 09:16:36 +0100

    Full example

--- example/eventsourced.php
+++ example/eventsourced.php
@@ -3,8 +3,15 @@
 namespace EventSourced;
 
 use Rhumsaa\Uuid\Uuid;
+
+use LiteCQRS\Repository;
+use LiteCQRS\DefaultCommand;
+use LiteCQRS\Commanding;
 use LiteCQRS\DefaultDomainEvent;
 use LiteCQRS\AggregateRoot;
+use LiteCQRS\EventStore;
+use LiteCQRS\Serializer\ReflectionSerializer;
+use LiteCQRS\Eventing;
 
 require_once __DIR__ . "/../vendor/autoload.php";
 
@@ -84,8 +91,84 @@ class InventoryItemDeactivated extends DefaultDomainEvent
     }
 }
 
-$item = new InventoryItem(Uuid::uuid4());
-$item->changeName("Foo");
-$item->deactivate();
+class CreateInventoryItem extends DefaultCommand
+{
+    public $id;
+}
 
-var_dump($item);
+class ChangeInventoryName extends DefaultCommand
+{
+    public $id;
+    public $newName;
+}
+
+class DeactivateInventoryItem extends DefaultCommand
+{
+    public $id;
+}
+
+class InventoryHandler
+{
+    private $repository;
+
+    public function __construct(Repository $repository)
+    {
+        $this->repository = $repository;
+    }
+
+    public function createInventoryItem(CreateInventoryItem $command)
+    {
+        $inventoryItem = new InventoryItem($command->id);
+
+        $this->repository->save($inventoryItem);
+    }
+
+    public function changeInventoryName(ChangeInventoryName $command)
+    {
+        $inventoryItem = $this->repository->find('EventSourced\\InventoryItem', $command->id);
+        $inventoryItem->changeName($command->newName);
+
+        $this->repository->save($inventoryItem);
+    }
+
+    public function deactivateInventoryItem(DeactivateInventoryItem $command)
+    {
+        $inventoryItem = $this->repository->find('EventSourced\\InventoryItem', $command->id);
+        $inventoryItem->deactivate();
+
+        $this->repository->save($inventoryItem);
+    }
+}
+
+class InventoryConsoleOutputListener
+{
+    public function onInventoryItemCreated(InventoryItemCreated $event)
+    {
+        echo $event->id;
+    }
+}
+
+$listener = new InventoryConsoleOutputListener();
+$eventHandlerLocator = new Eventing\MemoryEventHandlerLocator();
+$eventHandlerLocator->register($listener);
+
+$eventStore = new EventStore\OptimisticLocking\OptimisticLockingEventStore(
+    new EventStore\OptimisticLocking\MemoryStorage(),
+    new ReflectionSerializer()
+);
+$eventBus = new Eventing\SynchronousInProcessEventBus($eventHandlerLocator);
+
+$repository = new EventStore\EventSourceRepository($eventStore, $eventBus);
+
+$inventoryHandler = new InventoryHandler($repository);
+$commandHandlerLocator = new Commanding\MemoryCommandHandlerLocator();
+$commandHandlerLocator->register('EventSourced\CreateInventoryItem', $inventoryHandler);
+$commandHandlerLocator->register('EventSourced\ChangeInventoryName', $inventoryHandler);
+$commandHandlerLocator->register('EventSourced\DeactivateInventoryItem', $inventoryHandler);
+$commandBus = new Commanding\SequentialCommandBus($commandHandlerLocator);
+
+$id = Uuid::uuid4();
+
+$commandBus->handle(new CreateInventoryItem(array('id' => $id)));
+$commandBus->handle(new ChangeInventoryName(array('id' => $id, 'newName' => 'test')));
+$commandBus->handle(new DeactivateInventoryItem(arraY('id' => $id)));
